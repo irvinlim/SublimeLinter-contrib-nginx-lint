05992984bd9b1deb81a829a3226ff1e96d0ef44e
Irvin Lim <irvinlimweiquan@gmail.com>
2017-04-29 20:56:15 +0800

    Complete linter with regex support for warnings and errors

--- linter.py
+++ linter.py
@@ -16,21 +16,18 @@ from SublimeLinter.lint import Linter, util
 class NginxLint(Linter):
     """Provides an interface to nginx-lint."""
 
-    syntax = ''
+    syntax = 'nginx'
     cmd = 'nginx-lint'
     executable = None
-    version_args = '--version'
-    version_re = r'(?P<version>\d+\.\d+\.\d+)'
-    version_requirement = '>= 1.0'
-    regex = r''
-    multiline = False
-    line_col_base = (1, 1)
-    tempfile_suffix = None
+    regex = (
+        r'^\"?.+(?::|\" \(line )'                       # Matches `filename:` OR `"filename" (line `
+        r'(?P<line>\d+)'                                # Captures line number
+        r'(?::|, column )'                              # Matches `:` OR `, column `
+        r'(?P<col>\d+)'                                 # Captures column number
+        r'(?:\):\n| )'                                  # Matches `):\n` OR a space
+        r'(?:(?P<warning>[^\s]+): |(?P<error>.+)\n)'    # Captures warning before `: `, or error before `\n`
+        r'(?P<message>.+)'                              # Captures message
+    )
+    multiline = True                                    # Errors span 3 lines, while warnings span 1
+    tempfile_suffix = '.conf'                           # nginx-lint takes in a file name, rather than input from stdin
     error_stream = util.STREAM_BOTH
-    selectors = {}
-    word_re = None
-    defaults = {}
-    inline_settings = None
-    inline_overrides = None
-    comment_re = None
-
